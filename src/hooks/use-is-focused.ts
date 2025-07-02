f07e7b21855b905b9ca0b35aa98a59c33dfa2b1e
Anh Ngoc <ngocda@ows.com.vn>
2025-07-02 22:09:34 +0700

    ✨ Add useIsFocused hook to manage screen focus state

---
+++ src/hooks/use-is-focused.ts
@@ -0,0 +1,31 @@
+import { useNavigation } from '@react-navigation/native';
+import * as React from 'react';
+
+/**
+ * Hook to get the current focus state of the screen. Returns a `true` if screen is focused, otherwise `false`.
+ * This can be used if a component needs to render something based on the focus state.
+ */
+export function useIsFocused(): boolean {
+  const navigation = useNavigation();
+
+  const subscribe = React.useCallback(
+    (callback: () => void) => {
+      const unsubscribeFocus = navigation.addListener('focus', callback);
+      const unsubscribeBlur = navigation.addListener('blur', callback);
+
+      return () => {
+        unsubscribeFocus();
+        unsubscribeBlur();
+      };
+    },
+    [navigation],
+  );
+
+  const value = React.useSyncExternalStore(
+    subscribe,
+    navigation.isFocused,
+    navigation.isFocused,
+  );
+
+  return value;
+}
